dcf89f33e607f245f974b5f184dac751e25240bf
Dubier_Barrientos <129345580+Dubier-Barrientos@users.noreply.github.com>
2023-11-16 12:06:11 -0500

    Update app.py

--- app.py
+++ app.py
@@ -33,7 +33,9 @@ client1= paho.Client("ErnestoDoor")
 client1.on_message = on_message
 
 st.write(" !Hola, bienvenido a Eco-House! ")
-st.title(" !Hola, bienvenido a Eco-House! ", style={"font-weight": "bold", "font-style": "italic", "color": "#228B22"})
+st.markdown("""
+<h1 style="background-color: transparent; color: #228B22; font-weight: bold;"> !Hola, bienvenido a Eco-House! </h1>
+""")
 st.text(" La casa con sistema de cerradura inteligente ")
 
 
