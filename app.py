18107c7407c752832e00cd12540b4df730309736
Dubier_Barrientos <129345580+Dubier-Barrientos@users.noreply.github.com>
2023-11-16 11:05:20 -0500

    Update app.py

--- app.py
+++ app.py
@@ -2,6 +2,15 @@ import paho.mqtt.client as paho
 import time
 import streamlit as st
 import json
+from bokeh.models.widgets import Button
+from bokeh.models import CustomJS
+from streamlit_bokeh_events import streamlit_bokeh_events
+from PIL import Image
+import time
+import glob
+import paho.mqtt.client as paho
+import os
+
 values = 0.0
 act1="OFF"
 
@@ -23,7 +32,50 @@ port=1883
 client1= paho.Client("ErnestoDoor")
 client1.on_message = on_message
 
+st.write("Toca el Botón y habla ")
 
+stt_button = Button(label=" Inicio ", width=200)
+
+stt_button.js_on_event("button_click", CustomJS(code="""
+    var recognition = new webkitSpeechRecognition();
+    recognition.continuous = true;
+    recognition.interimResults = true;
+ 
+    recognition.onresult = function (e) {
+        var value = "";
+        for (var i = e.resultIndex; i < e.results.length; ++i) {
+            if (e.results[i].isFinal) {
+                value += e.results[i][0].transcript;
+            }
+        }
+        if ( value != "") {
+            document.dispatchEvent(new CustomEvent("GET_TEXT", {detail: value}));
+        }
+    }
+    recognition.start();
+    """))
+
+result = streamlit_bokeh_events(
+    stt_button,
+    events="GET_TEXT",
+    key="listen",
+    refresh_on_update=False,
+    override_height=75,
+    debounce_time=0)
+
+if result:
+    if "GET_TEXT" in result:
+        st.write(result.get("GET_TEXT"))
+        client1.on_publish = on_publish                            
+        client1.connect(broker,port)  
+        message =json.dumps({"Act1":result.get("GET_TEXT").strip()})
+        ret= client1.publish("voz_pedro", message)
+
+    
+    try:
+        os.mkdir("temp")
+    except:
+        pass
 
 st.title("MQTT Control")
 
@@ -52,16 +104,6 @@ if st.button('CIERRA'):
     
 else:
     st.write('')
-
-values = st.slider('Selecciona el rango de valores',0.0, 100.0)
-st.write('Values:', values)
-
-if st.button('Enviar valor analógico'):
-    client1= paho.Client("ErnestoDoor")                           
-    client1.on_publish = on_publish                          
-    client1.connect(broker,port)   
-    message =json.dumps({"Analog": float(values)})
-    ret= client1.publish("motor_de_ernesto", message)
     
  
 else:
